280340847b10c406ba9e2914f20b8ce0ba6585d7
smalldot <z0976530159@gmail.com>
2022-07-07 20:44:39 +0800

    second

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,34 @@
-console.log('Hello!');
+$('document').ready(function() {
+    // 打開選單
+    $('.mobile-link').click(function(event) {
+        event.preventDefault();
+        $('body').toggleClass('open');
+    });
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+    //glasses 選單 切換
+    $('.menu-opt').click(function(event) {
+        event.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('menu-active');
+        $(this).addClass('menu-active')
+        $(".sun").addClass('dn')
+        $(".fun").addClass('dn')
+        $(".opt").removeClass('dn')
+    });
+    $('.menu-sun').click(function(event) {
+        event.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('menu-active');
+        $(this).addClass('menu-active')
+        $(".opt").addClass('dn')
+        $(".fun").addClass('dn')
+        $(".sun").removeClass('dn')
+    });
+    $('.menu-fun').click(function(event) {
+        event.preventDefault();
+        $(this).parent().siblings().find('a').removeClass('menu-active');
+        $(this).addClass('menu-active')
+        $(".sun").addClass('dn')
+        $(".opt").addClass('dn')
+        $(".fun").removeClass('dn')
+    });
+    
 });
