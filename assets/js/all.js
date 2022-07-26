4e979debbf36bbb2e55379c1336ec5bd6e9439ee
smalldot <z0976530159@gmail.com>
2022-07-26 22:39:24 +0800

    Update 2022-07-26T14:39:18.119Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,32 +4,12 @@ $('document').ready(function () {
   // 打開選單
   $('.mobile-link').click(function (event) {
     event.preventDefault();
-    $('body').toggleClass('open');
-  }); //glasses 選單 切換
-
-  $('.menu-opt').click(function (event) {
-    event.preventDefault();
-    $(this).parent().siblings().find('a').removeClass('menu-active');
-    $(this).addClass('menu-active');
-    $(".sun").addClass('dn');
-    $(".fun").addClass('dn');
-    $(".opt").removeClass('dn');
+    $('.m-menu').toggleClass('d-none');
   });
-  $('.menu-sun').click(function (event) {
-    event.preventDefault();
-    $(this).parent().siblings().find('a').removeClass('menu-active');
-    $(this).addClass('menu-active');
-    $(".opt").addClass('dn');
-    $(".fun").addClass('dn');
-    $(".sun").removeClass('dn');
-  });
-  $('.menu-fun').click(function (event) {
-    event.preventDefault();
-    $(this).parent().siblings().find('a').removeClass('menu-active');
-    $(this).addClass('menu-active');
-    $(".sun").addClass('dn');
-    $(".opt").addClass('dn');
-    $(".fun").removeClass('dn');
+  $(".gl-menu-item").each(function () {
+    if ($(this)[0].href == String(window.location)) {
+      $(this).addClass("menu-active").parent().siblings().find("a").removeClass("menu-active");
+    }
   });
 });
 //# sourceMappingURL=all.js.map
